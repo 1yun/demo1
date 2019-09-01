564d56b9c6ed9e2fbdbabbf2a1fb77b617101927
1yun <2836765140@qq.com>
2019-09-01 20:03:47 +0800

    第二次

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("dsadsasd");
+        System.out.println("dfdasdsadsa");
         System.out.println("dsadsda");
         SpringApplication.run(DemoApplication.class, args);
     }
