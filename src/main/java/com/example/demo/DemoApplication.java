592186a187338d24772de204a997a641cfdd3b0b
1yun <2836765140@qq.com>
2019-09-02 22:11:47 +0800

    第三次

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,12 @@ public class DemoApplication {
         System.out.println("dfdasdsadsa");
         System.out.println("dsadsda");
         SpringApplication.run(DemoApplication.class, args);
+        System.out.println("dsadsasd");
+        System.out.println("dfdasdsadsa");
+        System.out.println("dsadsda");
+        SpringApplication.run(DemoApplication.class, args);
+
+        //如果建立分治 需要fetch 入第三方士大夫士大夫
     }
 
 }
